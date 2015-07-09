9ab0260ea583d2a4fb49dd219afcb0c8f7ae8e3e
Adam Harris <adam.jensen.harris@gmail.com>
2015-07-09 11:49:13 -0600

    Overrided onDisconnect

--- src/main/java/com/mycompany/twitchplays/TwitchRobo.java
+++ src/main/java/com/mycompany/twitchplays/TwitchRobo.java
@@ -50,30 +50,42 @@ public class TwitchRobo extends PircBot {
         long time = 0;
         
         while(true) {
-        if(input.get(message) != null)
-        {
-            time = System.currentTimeMillis();
-            try{
-                //create a new robot to place in inputs
-                Robot robo = new Robot();
-                robo.keyPress(input.get(message));
-                robo.delay(100);
-                robo.keyRelease(input.get(message));
-            }catch(Exception ex){
-                ex.printStackTrace();
-            }
-        } else if((time + thirtyMin) == System.currentTimeMillis()){
-            try {
-                Robot robos = new Robot();
-                robos.keyPress(input.get("b"));
-                robos.delay(100);
-                robos.keyRelease(input.get("b"));
-            } catch(Exception ex) {
-                ex.printStackTrace();
-            }
-            
+            if(input.get(message) != null)
+            {
+                time = System.currentTimeMillis();
+                try{
+                    //create a new robot to place in inputs
+                    Robot robo = new Robot();
+                    robo.keyPress(input.get(message));
+                    robo.delay(100);
+                    robo.keyRelease(input.get(message));
+                }catch(Exception ex){
+                    ex.printStackTrace();
+                }
+            } else if((time + thirtyMin) == System.currentTimeMillis()){
+                try {
+                    Robot robos = new Robot();
+                    robos.keyPress(input.get("b"));
+                    robos.delay(100);
+                    robos.keyRelease(input.get("b"));
+                } catch(Exception ex) {
+                    ex.printStackTrace();
+                }
         }
     }
   }
+    
+    @Override
+    public void onDisconnect(){
+        while (!isConnected()) {
+            try {
+                reconnect();
+            }
+            catch (Exception e) {
+                // Couldn't reconnect!
+                // Pause for a short while...?
+            }
+        }
+    }
     
 }
